c8a672c7d63c993e19414efcf9880e94f4e4a94f
EDUARDO DE OLIVEIRA <eduardof_oliveira@hotmail.com>
2025-12-13 21:45:32 +0000

    changes

---
+++ models/staging/sap_northwind/stg_nwind_products.sql
@@ -0,0 +1,22 @@
+with
+    source_products as (
+        select *
+        from {{ source('nwind', 'products') }}
+    )
+
+    , renamed as (
+        select
+            cast(product_id as int) as product_pk
+            , cast(supplier_id as int) as supplier_fk
+            , cast(category_id as int) as category_fk
+            , cast(product_name as string) as product_name
+            , cast(units_in_stock as int) as units_in_stock
+            , cast(unit_price as float) as unit_price
+            , cast(units_on_order as int) as units_on_order
+            , cast(quantity_per_unit as string) as quantity_per_unit
+            , cast(reorder_level as int) as reorder_level
+            , cast(discontinued as boolean) as is_discontinued
+        from source_products
+    )
+
+select * from renamed
